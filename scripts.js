fc93f63302258a6ca31e05b21899ef283f0ef991
shreypuranik <code@shreypuranik.co.uk>
2016-06-25 13:16:54 +0100

    Hiding stuff

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 $("#showDetailsSubmit").click(function(){
     var showName = $("#showNameInput").val();
     var showHost = $("#showHostInput").val();
+    $("#showDetailsDiv").empty();
     $("#showDetailsDiv").load("addShowDetails.php?showName="+showName+"&showHost="+showHost);
 })
 
