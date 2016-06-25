d513f76699724922bebcb2bbfffd338ed1175a08
shreypuranik <code@shreypuranik.co.uk>
2016-06-25 12:02:21 +0100

    Adding in index.php, also adding .gitignore

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <title>Radio Show Review System</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="scripts.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <style>
+        .headerbox{
+            background: #004c00;
+            color: white;
+        }
+        .bs_input{
+            width: 65%;
+        }
+        .footer{
+            background: #2c2d2e;
+            color: white;
+        }
+    </style>
+
+</head>
+<body>
+<div class="row headerbox">
+    <div class="col-sm-6 ">
+        <h3>&nbsp; Show Reviews</h3>
+    </div>
+    <div class="col-sm-6">
+    </div>
+</div>
+<div class="row mainpagebody">
+
+</div>
+<div class="row footer">
+    <div class="col-sm-6 ">
+<span>&nbsp; Built with love, using PHP &amp; MongoDB</span>
+</div>
+    <div class="col-sm-3">
+
+    </div>
+    <div class="col-sm-3 ">
+        <span>Pull requests welcome. Fork me on <a href="https://github.com/shreypuranik/radio-show-review" target="_blank">GitHub</a>.</span>
+    </div>
+    </div>
+
+</body>
+</html>
