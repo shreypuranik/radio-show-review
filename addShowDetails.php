1fa4111a8a81c75a11b9ae2e24c61e7313b8dbd6
shreypuranik <code@shreypuranik.co.uk>
2016-06-25 13:31:11 +0100

    Adding in Data class integration to add new show details

--- addShowDetails.php
+++ addShowDetails.php
@@ -1,2 +1,14 @@
 <?php
-echo "Thanks!";
+include_once("class.RSRData.php");
+
+if (!empty($_GET['showName'])
+    && !empty($_GET['showHost']))
+{
+    $rsrData = new RSRData();
+    $rsrData->addShowToCollection($_GET['showName'], $_GET['showHost']);
+    echo '<p>Thanks. This is now in the system</p>';
+}
+else{
+    echo '<p>Hmm. Not quite right! Try again.</p>';
+}
+?>
